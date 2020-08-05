8bdaa08e7ff49f0dad0bfbe6a5d266c89cf6125f
khoalun <buikhoa793@gmail.com>
2020-08-05 18:58:44 +0300

    TrafficLight

--- src/components/TrafficLight.js
+++ src/components/TrafficLight.js
@@ -9,14 +9,19 @@ const GREEN = 2;
 class TrafficLight extends Component {
     constructor() {
         super();
-        this.currentColor = GREEN;
+        this.state = {
+            currentColor: GREEN
+        };
         console.log(this.currentColor);
 
         setInterval(() => {
             console.log(this.currentColor);
-            this.currentColor = this.getNextColor(this.currentColor);
+            this.setState({
+                currentColor: this.getNextColor(this.state.currentColor)
+            });
         }, 1000);
     }
+
     getNextColor(currentColor) {
         switch (currentColor) {
             case RED:
@@ -30,18 +35,22 @@ class TrafficLight extends Component {
     }
 
     render() {
+        const { currentColor } = this.state;
+        console.log('Rendering...', currentColor);
         return <div className='TrafficLight'>
+
+
             {/* <div className="bulb red" />
             <div className="bulb orange" />
             <div className="bulb green" /> */}
             <div className={classNames('bulb', 'red', {
-                active: this.currentColor === RED
+                active: currentColor === RED
             })} />
             <div className={classNames('bulb', 'orange', {
-                active: this.currentColor === ORANGE
+                active: currentColor === ORANGE
             })} />
             <div className={classNames('bulb', 'green', {
-                active: this.currentColor === GREEN
+                active: currentColor === GREEN
             })} />
 
 
